eea58655b51ca36fd13ed9aa2433357ddc96d917
jh.lee <nimbus9030@gmail.com>
2018-05-30 23:37:35 +0900

    done

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -10,86 +10,157 @@
  * 20180521 簡単に動作するjs
  *
  */
-var insta_data;
+// var insta_data;
 
-function get(url) {
-  // Return a new promise.
-  return new Promise(function(resolve, reject) {
-    // Do the usual XHR stuff
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
+// function get(url) {
+//   // Return a new promise.
+//   return new Promise(function(resolve, reject) {
+//     // Do the usual XHR stuff
+//     var req = new XMLHttpRequest();
+//     req.open('GET', url);
 
-    req.onload = function() {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(req.response);
-      }
-      else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
-      }
-    };
+//     req.onload = function() {
+//       // This is called even on 404 etc
+//       // so check the status
+//       if (req.status == 200) {
+//         // Resolve the promise with the response text
+//         resolve(req.response);
+//       }
+//       else {
+//         // Otherwise reject with the status text
+//         // which will hopefully be a meaningful error
+//         reject(Error(req.statusText));
+//       }
+//     };
 
-    // Handle network errors
-    req.onerror = function() {
-      reject(Error("Network Error"));
-    };
+//     // Handle network errors
+//     req.onerror = function() {
+//       reject(Error("Network Error"));
+//     };
 
-    // Make the request
-    req.send();
-  });
-}
+//     // Make the request
+//     req.send();
+//   });
+// }
 
 
+// $(function(){
+//     //pure js
+//     sessionStorage.clear();
+//     var token = '3520061443.a090108.3d61d228cb6f4c77a3a39e1b1d8674ed',
+//         num_photos = 10,
+//         // tag_name = '春',
+//         container = document.getElementById( 'rudr_instafeed' ),
+//         scrElement = document.createElement( 'script' );
+//     var url = 'https://www.instagram.com/explore/tags/' + "coffee" + '/?__a=1';
+
+//     get(url)
+//     .then(function(response) {
+//         insta_data = JSON.parse(response);
+//       }, function(error) {
+//         console.error("Failed!", error);
+//     })
+//     .then(function(){
+//       for( x in insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges ){
+//         var edge = insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges[x];
+//         var shortcode = edge.node.shortcode;
+//         var url2 = 'https://api.instagram.com/oembed/?url=http://instagr.am/p/' + shortcode;
+//         // console.log( edge );
+//         get(url2)
+//         .then(function(response) {
+//           // insta_data2.push( JSON.parse(response) );
+
+//           var data = JSON.parse(response);
+//           // console.log(  JSON.parse(response)  );
+//           sessionStorage.setItem( data.author_id, data.author_name );
+//         }, function(error) {
+//           console.error("Failed!", error);
+//         })
+//       }
+//     })
+//     .then(function(){
+//       for( x in insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges ){
+//         var edge = insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges[x];
+//         var thumbnail = edge.node.thumbnail_src;
+//         var comment_count = edge.node.edge_media_to_comment.count;
+//         var like_count = edge.node.edge_liked_by.count;
+//         var shortcode = edge.node.shortcode;
+//         var id = edge.node.owner.id;
+//         var author_name = sessionStorage.getItem(id);
+//         console.log(author_name);
+//         $('ul').append('<a href="https://www.instagram.com/p/'+ shortcode + '/?taken-by='+ author_name +'" target="blank">@ '+ author_name  + '</a>');
+//         // $('ul').append('<li id= "' + x + '" class="list-inline-item"><img src="'+ thumbnail +'" width="300" height="300" onmouseover="" ><div><a href="https://www.instagram.com/p/'+ shortcode + '/?taken-by='+ author_name +'" target="blank">@ '+ author_name  + '</a></div><div>like '+ like_count  + '</div><div>comment '+ comment_count + '</div></li>');
+//       }
+//     })
+//   });
+
+//// ajax model
 $(function(){
-    //pure js
-    sessionStorage.clear();
-    
-    var token = '3520061443.a090108.3d61d228cb6f4c77a3a39e1b1d8674ed',
-        num_photos = 10,
-        // tag_name = '春',
-        container = document.getElementById( 'rudr_instafeed' ),
-        scrElement = document.createElement( 'script' );
-    var url = 'https://www.instagram.com/explore/tags/' + tag + '/?__a=1';
+    var url = 'https://www.instagram.com/explore/tags/' + "coffee" + '/?__a=1';
+    var token = '3520061443.a090108.3d61d228cb6f4c77a3a39e1b1d8674ed';
 
-    get(url)
-    .then(function(response) {
-        insta_data = JSON.parse(response);
-      }, function(error) {
-        console.error("Failed!", error);
-    })
-    .then(function(){
-      for( x in insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges ){
-        var edge = insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges[x];
+	$.ajax({
+		url: url ,
+		data : $("#taApiParam").val(),
+		dataType : "JSON",
+		/*
+		type : method,
+		*/
+		success : function(json){
+			// console.log("step1 : get tag",json);
+	    // $('ul').append('| ajax model |');
+			$(json.graphql.hashtag.edge_hashtag_to_top_posts.edges).each(function(){
+        var edge = this;
         var shortcode = edge.node.shortcode;
         var url2 = 'https://api.instagram.com/oembed/?url=http://instagr.am/p/' + shortcode;
-        // console.log( edge );
-        get(url2)
-        .then(function(response) {
-          // insta_data2.push( JSON.parse(response) );
-
-          var data = JSON.parse(response);
-          // console.log(  JSON.parse(response)  );
-          sessionStorage.setItem( data.author_id, data.author_name );
-        }, function(error) {
-          console.error("Failed!", error);
-        })
-      }
-    })
-    .then(function(){
-      for( x in insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges ){
-        var edge = insta_data.graphql.hashtag.edge_hashtag_to_top_posts.edges[x];
+        
+				// console.log("step2 : set oembed ", edge);
+        // setList(shortcode, url2);
+		    setList(edge, url2);    
+        /*
+        
         var thumbnail = edge.node.thumbnail_src;
         var comment_count = edge.node.edge_media_to_comment.count;
         var like_count = edge.node.edge_liked_by.count;
-        var shortcode = edge.node.shortcode;
         var id = edge.node.owner.id;
         var author_name = sessionStorage.getItem(id);
-        console.log(author_name);
-        $('ul').append('<li id= "' + x + '" class="list-inline-item"><img src="'+ thumbnail +'" width="300" height="300" onmouseover="" ><div><a href="https://www.instagram.com/p/'+ shortcode + '/?taken-by='+ author_name +'" target="blank">@ '+ author_name  + '</a></div><div>like '+ like_count  + '</div><div>comment '+ comment_count + '</div></li>');
-      }
-    })
-  });
+        $('ul').append('<a href="https://www.instagram.com/p/'+ shortcode + '/?taken-by='+ author_name +'" target="blank">@ '+ author_name  + '</a>');
+				*/
+			});
+		} ,
+		error : function(err){
+      alert("오류가 발생하였습니다. 관리자에 문의하세요.");
+			console.log(err);
+		}
+	});
+	
+	// function setList(shortcode,url){
+    function setList(edge, url) {
+      $.ajax({
+        url: url ,
+        data : $("#taApiParam").val(),
+        dataType : "JSON",
+        /*
+        type : method,
+        */
+       success : function(json){
+         // console.log("step3 : get oembed",json);
+         var thumbnail = json.thumbnail_url;
+         var id = json.author_id;
+         var author_name = json.author_name;
+         var shortcode = edge.node.shortcode;
+         var comment_count = edge.node.edge_media_to_comment.count;
+         var like_count = edge.node.edge_liked_by.count;
+  
+         $('ul').append('<li id= "' + id + '" class="list-inline-item"><img src="' + thumbnail + '" width="300" height="300" onmouseover="" ><div><a class="social-link rounded-circle text-white mr-3" href="https://www.instagram.com/p/' + shortcode + '/?taken-by=' + author_name + '" target="blank">@ ' + author_name + '</a></div><div>like ' + like_count + '</div><div>comment ' + comment_count + '</div></li>');
+        //  $('ul').append('<li id= "' + id + '" class="list-inline-item"><img src="' + thumbnail + '" width="300" height="300" onmouseover="" ><div><a class="social-link rounded-circle text-white mr-3" href="https://www.instagram.com/p/' + shortcode + '/?taken-by=' + author_name + '" target="blank">@ ' + author_name + '</a></div></li>');
+        // $('ul').append('<a href="https://www.instagram.com/p/'+ shortcode + '/?taken-by='+ author_name +'" target="blank"><img src="'+thumbnail+'">@ '+ author_name  + '</a>');
+			} ,
+			error : function(err){
+				alert("오류가 발생하였습니다. 관리자에 문의하세요.");
+				console.log(err);
+			}
+		});		
+	}
+	
+});
